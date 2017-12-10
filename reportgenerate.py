3de2201cc375cfd6dace2f527f41f42829546d76
NJ-zero <821854810@qq.com>
2017-12-10 16:37:22 +0800

    指定文件路径并生成文件

--- reportgenerate.py
+++ reportgenerate.py
@@ -5,13 +5,11 @@
 import report
 import xlsxwriter
 
-
 def Report():
     workbook = xlsxwriter.Workbook('D:/file/android.xlsx')
     bo = report.GenerateReport(workbook)
     bo.writedata()
     bo.close()
-
 
 if __name__ == "__main__":
     Report()
